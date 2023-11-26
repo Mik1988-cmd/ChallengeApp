ca1c3b4c71a1c654b6f41b89678d0dc11a0b120e
Mikołaj Ostrowski <mikostr88@gmail.com>
2023-11-26 23:56:16 +0100

    zadanie domowe z dnia 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,1 +1,37 @@
-﻿Console.WriteLine("Hello, World!");
+﻿//zadeklaruj zmienna z imieniem
+var name = "Ewa";
+//zadeklaruj zmienna z plcia
+var gender = "Kobieta";
+//zadeklaruj zmienna z wiekem
+var age = 33;
+//zweryfikuj dane i wyswietl ponizsze komunikaty
+//kobieta ponizej 30 lat
+//Ewa ma 33 lata
+//niepelnoletni mezczyzna
+
+if (age == 30 && name == "Ewa")
+{
+    Console.WriteLine("Ewa ma 33 lata");
+}
+else if (gender == "Kobieta")
+{
+    if (age < 30)
+    {
+        Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+    else
+    {
+        Console.WriteLine("Kobieta powyzej 30 lat");
+    }
+}
+else
+{
+    if (age < 18)
+    {
+        Console.WriteLine("Niepelnotetni mezczyzna");
+    }
+    else
+    {
+        Console.WriteLine("Pelnoletni mezczyzna");
+    }
+}
